5dcd5edf515515708fb5c4d4db61dde2ca57b71c
sagar-gopal-developer <sagrgopal509045@gmail.com>
2026-06-18 14:32:30 +0530

    Setup PostgreSQL and create user model

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.database import engine, Base
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "URL Shortener API"}
