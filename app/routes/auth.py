b23ac7956c0176a5b1f53bf1715f4f13c6c0ada4
sagar-gopal-developer <sagrgopal509045@gmail.com>
2026-06-22 11:14:31 +0530

    Implement JWT token generation

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate
+from app.utils.security import hash_password
+from fastapi import HTTPException
+from app.schemas.user import UserCreate, UserLogin
+from app.utils.security import hash_password, verify_password
+from app.utils.jwt import create_access_token
 
 router = APIRouter()
 
@@ -16,10 +21,24 @@ def test():
 @router.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
 
+    existing_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    hashed_password = hash_password(user.password)
+
     new_user = User(
         username=user.username,
         email=user.email,
-        password=user.password
+        password=hashed_password
     )
 
     db.add(new_user)
@@ -30,3 +49,36 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
         "message": "User registered successfully",
         "user_id": new_user.id
     }
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    existing_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+        user.password,
+        existing_user.password
+    ):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid email or password"
+        )
+    
+    token = create_access_token(
+    {"sub": str(existing_user.id)}
+    )
+
+    return {
+    "access_token": token,
+    "token_type": "bearer"
+    } 
---
+++ app/utils/jwt.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "your_super_secret_key_here"
+ALGORITHM = "HS256"
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=30)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
